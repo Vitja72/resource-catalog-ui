9633f40070b8921f7fadf22116fafbe8a544d5af
Vitja72 <viktor.stefan@tn.techstarter.de>
2025-08-25 13:03:06 +0200

    feat(UI-104): ResourceCard Komponente entwickelt

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import './App.css'
+import ResourceCard from './components/RessourceCard.jsx'
+
 function App() {
+
+  const dummyResource = null;
+  
   return (
-    
-     <div className="min-h-screen bg-gray-100 font-sans antialiased">
-      <header className="bg-gradient-to-r from-blue-600 to-indigo-700 p-4 shadow-lg">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-3xl font-extrabold text-white">Ressourcen-katalog</h1>
-          <nav>
-            {/*Hier kommt später das Menü. */}
-          </nav>
+      <div className="min-h-screen bg-gray-100 font-sans antialiased">
+        <header className="bg-gradient-to-r from-blue-600 to-indigo-700 p-4 shadow-lg">
+          <div className="container mx-auto flex justify-between items-center">
+            <h1 className="text-3xl font-extrabold text-white">
+              Ressourcen-Katalog
+            </h1>
+            <nav>
+              {/* Hier kommt spaeter das Menu. */}
+            </nav>
+          </div>
+        </header>
 
-        </div>
-
-
-      </header>
-      
-      <main className="container mx-auto p-4 mt-4">
-        <p className="text-gray-700 text-lg"> Willkommen im Ressourcen-Katalog!</p>
-
-
-      </main>
-      </div>     
-    
+        <main className="container mx-auto p-4 mt-4">
+          <div className="max-w-md">
+            <ResourceCard resource={dummyResource} />
+          </div>
+        </main>
+      </div>
   )
 }
+
 export default App
